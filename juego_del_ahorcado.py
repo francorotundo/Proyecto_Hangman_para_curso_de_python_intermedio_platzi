99d165c5b6f11c7e194832f4d1f69a4ed7fcd347
Francisco Rotundo <francorotundo90@gmail.com>
2022-05-03 19:25:57 -0400

    Se creo el algoritmo para que el usuario pueda adivinar la palabra, y avise si ganó o perdió.

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -1,30 +1,62 @@
-# ¡Adivina la palabra
-# ------
-# Ingresa una letra: 
-# ¡Ganaste! La palabra es -----
-# Incorpora comprehension, manejo de errores y manejo de archivos.
-# Utiliza el archivo data.txt y leelo para obtener las palabras.
-# Investiga la funcion enumerate. 
-# El metodo get de los diccionarios puede servirte.
-# La sentencia: os.system('cls') -> windows, os.system('clear') -> UNIX, te servira para limpiar la pantalla.
-# Mejora el juego: añade un sistema de puntos, dibuja al 'ahorcado' en cada jugada con codigo ASCII.
-# Mejora la interfaz.
-import random 
+import random
+import os
 random.seed()
-
+palabra = ''
+character = []
+adivinando = []
 
 def extractor_de_palabras():
+    global palabra
+    global x
     with open('./files/data.txt', 'r', encoding='utf-8') as f:
-        palabras = []
-        for line in f:
-            palabras.append(line)
+        palabras = [line for line in f]
         palabra = palabras[random.randint(0, 170)]
-        
+        for ch in range(0, len(palabra)-1):
+            character.append(palabra[ch])
+        for ch in character:    
+            adivinando.append('-')
+
+def adivina(letra = ''): 
+    x = 0 
+    y = 0 
+    fallos = 0
+
+    for i in adivinando:
+        x += 1
+        print(i, end='')
+
+    while x > y :
+        letra = input('\n\nIngresa una letra: ')
+        y = 0
+        if letra is not character:
+            fallos += 1 
+            if fallos == 11:
+                break
+
+        for i, ch in enumerate(character):           
+            if ch == letra:
+                adivinando[i] = ch
+        print('')
+        os.system('clear')
+        for i in adivinando:
+            if i != '-':
+                y += 1
+            print(i, end='')
+
+    if fallos == 11:
+        os.system('clear')
+        print('Perdiste\n')
+    else: 
+        print("\n\n¡GANASTE! la palabra es ", end='')
+        for i in adivinando:
+            print(i, end='')
+        print()
+
 
 def run():
-    print('¡Adivina la palabra!')
+    print('¡Adivina la palabra!\n')
     extractor_de_palabras()
-    # letra = input('Ingresa una letra: ')
+    adivina()
 
 
 if __name__=='__main__':
