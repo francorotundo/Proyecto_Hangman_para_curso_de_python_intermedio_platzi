e664c1d48f594b1b480a167517a164c049a7b328
Francisco Rotundo <francorotundo90@gmail.com>
2022-05-03 23:14:44 -0400

    Mejorando la interfaz para que sea mas entretenido para el usuario, ahora se dibuja el ahorcado

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -4,6 +4,20 @@ random.seed()
 palabra = ''
 character = []
 adivinando = []
+errorCero = ''
+errorUno ='\n               \n              O'
+errorDos ='\n               \n              O\n             /'
+errorTres ='\n               \n              O\n             / \\'
+errorCuatro ='\n               \n              O\n             /|\\\n              |'
+errorCinco ='\n               \n              O\n             /|\\\n              |\n             /'
+errorSeis ='\n               \n              O\n             /|\\\n              |\n             / \\'
+errorSiete ='\n               \n              O\n             /|\\\n              |\n_____________/_\\___'
+errorOcho ='\n   |           \n   |          O\n   |         /|\\\n   |          |\n___|_________/_\\___'
+errorNueve ='   ___________\n   |           \n   |          O\n   |         /|\\\n   |          |\n___|_________/_\\___'
+errorDiez ='   ___________\n   |          |\n   |          O\n   |         /|\\\n   |          |\n___|_________/_\\___'
+errorOnce ='   ___________\n   |          |\n   |          X\n   |         /|\\\n   |          |\n___|________ / \\'
+
+errores = [errorCero, errorUno, errorDos, errorTres, errorCuatro, errorCinco, errorSeis, errorSiete, errorOcho, errorNueve, errorDiez, errorOnce]
 
 def extractor_de_palabras():
     global palabra
@@ -12,7 +26,7 @@ def extractor_de_palabras():
         palabras = [line for line in f]
         palabra = palabras[random.randint(0, 170)]
         for ch in range(0, len(palabra)-1):
-            character.append(palabra[ch])
+            character.append(palabra[ch].upper())
         for ch in character:    
             adivinando.append('-')
 
@@ -28,13 +42,13 @@ def adivina(letra = ''):
     while x > y :
         letra = input('\n\nIngresa una letra: ')
         y = 0
-        if letra is not character:
+        if letra.upper() not in character:
             fallos += 1 
             if fallos == 11:
                 break
 
         for i, ch in enumerate(character):           
-            if ch == letra:
+            if ch == letra.upper():
                 adivinando[i] = ch
         print('')
         os.system('clear')
@@ -42,18 +56,31 @@ def adivina(letra = ''):
             if i != '-':
                 y += 1
             print(i, end='')
+        print('\n')
+        print(errores[fallos])
+
 
     if fallos == 11:
         os.system('clear')
         print('Perdiste\n')
-    else: 
-        print("\n\n¡GANASTE! la palabra es ", end='')
+        print(errorOnce)
+        print()
+    else:
+        print("\n\n¡GANASTE!, la palabra es ", end='')
         for i in adivinando:
             print(i, end='')
         print()
 
 
 def run():
+    print("""
+    ***    ***      ****      ****   ***   **********  ****     ****      ****      ****   ***
+    ***    ***     **  **     *****  ***   ****   ***  *****   *****     **  **     *****  ***
+    **********    ********    ****** ***   ***         ****** ******    ********    ****** ***
+    **********    ********    *** ******   ***  *****  *** ***** ***    ********    *** ******
+    ***    ***   ***    ***   ***  *****   ***    ***  ***  ***  ***   ***    ***   ***  *****
+    ***    ***  ***      ***  ***   ****   **********  ***       ***  ***      ***  ***   ****
+    """)
     print('¡Adivina la palabra!\n')
     extractor_de_palabras()
     adivina()
